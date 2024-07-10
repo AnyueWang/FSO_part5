83b25c759d18d32b99c8ad071ca8db638f49a8bf
AnyueWang <moonwong.way@gmail.com>
2024-07-10 11:14:16 +0200

    add: toggle visibility of new blog form

--- bloglist-frontend/src/components/BlogsPage.jsx
+++ bloglist-frontend/src/components/BlogsPage.jsx
@@ -1,35 +1,24 @@
 import Blog from './Blog'
+import NewBlog from './NewBlog'
+import NewBlogForm from './NewBlogForm'
 
-const BlogsPage = ({ blogs, user, newBlogInfo, onChangeNewBlogInfo, onClickLogout, onClickCreate }) => {
-    return (
-      <div>
-        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-          <p>{user.name} logged in.</p>
-          <button onClick={onClickLogout}>Logout</button>
-        </div>
-        <h2>Create a new blog</h2>
-        <div>
-          <form>
-            <div>
-              <label htmlFor="title">Title:</label>
-              <input type="text" id='title' onChange={onChangeNewBlogInfo.onChangeTitle} value={newBlogInfo.title} />
-            </div>
-            <div>
-              <label htmlFor="author">Author:</label>
-              <input type="text" id='author' onChange={onChangeNewBlogInfo.onChangeAuthor} value={newBlogInfo.author} />
-            </div>
-            <div>
-              <label htmlFor="url">Url:</label>
-              <input type="text" id='url' onChange={onChangeNewBlogInfo.onChangeUrl} value={newBlogInfo.url} />
-            </div>
-            <button type='submit' onClick={onClickCreate}>Create</button>
-          </form>
-        </div>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
+const BlogsPage = ({ blogs, user, onClickLogout, createBlog }) => {
+  return (
+    <div>
+      <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+        <p>{user.name} logged in.</p>
+        <button onClick={onClickLogout}>Logout</button>
       </div>
-    )
-  }
+      <NewBlog>
+        <NewBlogForm
+          createBlog={createBlog}
+        />
+      </NewBlog>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
 
-  export default BlogsPage
+export default BlogsPage
---
+++ bloglist-frontend/src/components/NewBlog.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const NewBlog = (props) => {
+    const [createNewBlogVisible, setCreateNewBlogVisible] = useState(false)
+    const hideWhenVisible = { display: createNewBlogVisible ? 'none' : '' }
+    const showWhenVisible = { display: createNewBlogVisible ? '' : 'none' }
+    const toggleNewNote = () => setCreateNewBlogVisible(!createNewBlogVisible)
+
+    return (
+        <div>
+            <div style={showWhenVisible}>
+                {props.children}
+                <button onClick={toggleNewNote}>Cancel</button>
+            </div>
+            <div style={hideWhenVisible}>
+                <button onClick={toggleNewNote}>New Blog</button>
+            </div>
+            <p></p>
+        </div>
+    )
+}
+
+export default NewBlog
